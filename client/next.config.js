26918b66d5cd0ee9ae0f562858df7acb45a208e0
Kaung San Phyoe <davidkaung@gmail.com>
2026-06-26 20:17:45 +0630

    Fix static frontend start script

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,12 @@
+const clientRoot = __dirname;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  turbopack: {
+    root: clientRoot,
+  },
+  outputFileTracingRoot: clientRoot,
 };
 
 module.exports = nextConfig;
